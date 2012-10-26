xiaosuo/snoopy

commit 7bf3992ff6418b38dcfb25abeeca61a7c556bf9d

Support responses without content-length

src/snoopy.c
@@ -139,8 +139,6 @@ static void flow_ctx_free_data(struct flow_ctx *fc)
 		stlist_del_head(&fc->req_list, r, link);
 		http_req_free(r);
 	}
-	if (fc->req_part)
-		http_req_free(fc->req_part);
 	if (fc->sch_ctx)
 		patn_sch_ctx_free(fc->sch_ctx);
 	if (fc->http_ctx)
@@ -408,8 +406,12 @@ static void save_path(const char *method, const char *path,
 	struct http_user *hu = user;
 	struct flow_ctx *fc = hu->fc;
 
-	if (!fc->req_part && !(fc->req_part = http_req_alloc()))
-		goto err;
+	if (!fc->req_part) {
+		fc->req_part = http_req_alloc();
+		if (!fc->req_part)
+			goto err;
+		stlist_add_tail(&fc->req_list, fc->req_part, link);
+	}
 
 	assert(fc->req_part->path == NULL);
 
@@ -448,12 +450,9 @@ static void end_req(void *user)
 {
 	struct http_user *hu = user;
 	struct flow_ctx *fc = hu->fc;
-	struct http_req *r;
 
-	if ((r = fc->req_part)) {
+	if (fc->req_part)
 		fc->req_part = NULL;
-		stlist_add_tail(&fc->req_list, r, link);
-	}
 }
 
 /*

src/http.c
@@ -162,6 +162,8 @@ enum {
 	HTTP_CE_DEFLATE,
 };
 
+#define HTTP_BODY_LEN_INFI	0xffffffffffffffffULL
+
 struct http_parse_ctx_common {
 	int			state;
 	int			minor_state;
@@ -228,6 +230,8 @@ void http_parse_ctx_free(http_parse_ctx_t *ctx)
 			inflateEnd(ctx->common[dir].streamp);
 			free(ctx->common[dir].streamp);
 		}
+		if (ctx->common[dir].body_len == HTTP_BODY_LEN_INFI)
+			g_http_stat.good++;
 	}
 	free(ctx);
 }
@@ -491,6 +495,9 @@ static int http_parse_header_field(http_parser_t *pasr,
 	} else if (fn_len == 16 && strcasecmp(hdr, "Content-Encoding") == 0) {
 		if (http_parse_content_encoding(c, fv))
 			goto err;
+	} else if (fn_len == 10 && strcasecmp(hdr, "Connection") == 0) {
+		if (strcasecmp(fv, "close") == 0 && c->body_len == 0)
+			c->body_len = HTTP_BODY_LEN_INFI;
 	}
 
 	call_header_field_handler(pasr, dir, hdr, fn_len, fv, fv_len, user);
@@ -686,7 +693,8 @@ static int __http_parse(http_parser_t *pasr, struct http_parse_ctx_common *c,
 	case HTTP_STATE_MSG_BODY:
 		assert(c->body_len > 0);
 		n = MIN(len, c->body_len);
-		c->body_len -= n;
+		if (c->body_len != HTTP_BODY_LEN_INFI)
+			c->body_len -= n;
 		if (decode_content(pasr, c, dir, data, n, user))
 			goto err;
 		if (c->body_len == 0) {

test/case/no-content-length/keywords.conf
@@ -1,0 +1,1 @@
+infrastructure

test/case/no-content-length/log.log
@@ -1,0 +1,6 @@
+2012-09-13T15:06:16.268956Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure
+2012-09-13T15:06:16.268956Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure
+2012-09-13T15:06:16.268956Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure
+2012-09-13T15:06:16.268956Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure
+2012-09-13T15:06:16.268956Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure
+2012-09-13T15:06:16.600278Z 10.13.150.1 149.20.20.133 http://www.kernel.org/ infrastructure

commit a40026fb2511e0cf196a598048fc79d47df821a0

Reset req_part if the last request is freed
Commit 7bf3992ff6418b38dcfb25abeeca61a7c556bf9d introduced this bug.

src/snoopy.c
@@ -695,6 +695,8 @@ static void end_res(void *user)
 		goto err;
 	if ((r->status_code / 100) != 1) {
 		stlist_del_head(&fc->req_list, r, link);
+		if (fc->req_part == r)
+			fc->req_part = NULL;
 		http_req_free(r);
 		if (fc->sch_ctx)
 			patn_sch_ctx_reset(fc->sch_ctx);