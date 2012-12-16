582531485e4842aa737c49fc5975610570fe8861
Changli Gao <xiaosuo@gmail.com>
2012-12-16 23:05:21 +0800

    Add a simple script for FQDN auditting

---
+++ contrib/snoopy-fqdn-audit.rb
@@ -0,0 +1,224 @@
+#!/usr/bin/env ruby
+#
+# Snoopy - A lightweight bypass censorship system for HTTP
+# Copyright (C) 2012- Changli Gao <xiaosuo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'thread'
+require 'net/http'
+require 'optparse'
+
+options = {}
+opt_pasr = OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.separator("")
+  opts.separator("Options:")
+
+  opts.on("-b", "--background", "Run as a background daemon") do |b|
+    options[:background] = true
+  end
+
+  opts.on('-H', '--host HOST', 'Server host') do |h|
+    options[:host] = h
+  end
+
+  opts.on("-h", "--help", "Show this message") do |h|
+    puts opts
+    exit
+  end
+end
+opt_pasr.parse!(ARGV)
+if ARGV.length > 0
+  puts opt_pasr
+  exit(false)
+end
+if options[:host]
+  HOST = options[:host]
+else
+  HOST = '127.0.0.1'
+end
+
+WHITE_LIST = '/etc/snoopy/snoopy-fqdn-white.list'
+GRAY_LIST = '/etc/snoopy/snoopy-fqdn-gray.list'
+BLACK_LIST = '/etc/snoopy/snoopy-fqdn-black.list'
+INPUT_LOG_FN = '/var/log/snoopy-fqdn.log'
+GRAY_LOG_FN = '/var/log/snoopy-fqdn-gray.log'
+BLACK_LOG_FN = '/var/log/snoopy-fqdn-black.log'
+WHITE_URI = "http://#{HOST}/getlist.php?type=1"
+GRAY_URI = "http://#{HOST}/getlist.php?type=2"
+BLACK_URI = "http://#{HOST}/getlist.php?type=3"
+GRAY_POST_URI = "http://#{HOST}/postlist.php?type=2"
+BLACK_POST_URI = "http://#{HOST}/postlist.php?type=3"
+POST_INTERVAL = 10 * 60 # 10 minutes
+SYNC_INTERVAL = 10 * 60 # 10 minutes
+
+module FQDN
+  class Label
+    attr_accessor :ts
+
+    def initialize
+      @sub_label = {}
+    end
+
+    def [](label)
+      @sub_label[label]
+    end
+
+    def []=(label, sub)
+      @sub_label[label] = sub
+    end
+  end
+
+  class Database
+    def initialize(fn)
+      @root = FQDN::Label.new
+      IO.foreach(fn) do |line|
+        insert(line.rstrip)
+      end
+    end
+
+    def insert(fqdn, ts = nil)
+      ts ||= Time.new - POST_INTERVAL
+      cur = @root
+      fqdn.split(/\./).reverse.each do |label|
+        cur[label] ||= FQDN::Label.new
+        cur = cur[label]
+      end
+      cur.ts = ts
+    end
+
+    def query(fqdn)
+      cur = @root
+      fqdn.split(/\./).reverse.each do |label|
+        return nil unless cur = cur[label]
+        return cur if cur.ts
+      end
+      nil
+    end
+  end
+end
+
+class LogPoster
+  def initialize(uri, log_path)
+    @queue = Queue.new
+    Thread.new do
+      uri = URI.parse(uri)
+      loop do
+        fqdn = @queue.shift
+	# Why not use post_form, because post_form doesn't reserve query
+	req = Net::HTTP::Post.new(uri.path + '?' + uri.query)
+	req.form_data = {'fqdn' => fqdn}
+	Net::HTTP.new(uri.host, uri.port).start do |http|
+	  http.request(req)
+	end
+      end
+    end
+    @log = File.open(log_path, 'a')
+  end
+
+  def log(ts, fqdn)
+    log = ts.strftime('%FT%T') + ('.%06u' % ts.usec) + ' ' + fqdn + "\n"
+    @log.write(log)
+    @queue << fqdn
+  end
+end
+
+# Create the input log file as a pipe
+unless File.pipe?(INPUT_LOG_FN)
+  File.unlink(INPUT_LOG_FN) if File.exist?(INPUT_LOG_FN)
+  `mkfifo #{INPUT_LOG_FN}`
+  unless File.pipe?(INPUT_LOG_FN)
+    puts 'failed to create ' + INPUT_LOG_FN
+    exit(1)
+  end
+end
+
+if options[:background]
+  exit!(0) if fork
+  Process.setsid
+  exit!(0) if fork
+  Dir.chdir('/')
+  File.umask(0)
+  STDIN.reopen('/dev/null')
+  STDOUT.reopen('/dev/null', 'w')
+  STDERR.reopen('/dev/null', 'w')
+end
+
+# Create two log posters for black and gray logs
+black_log = LogPoster.new(BLACK_POST_URI, BLACK_LOG_FN)
+gray_log = LogPoster.new(GRAY_POST_URI, GRAY_LOG_FN)
+white_list = nil
+gray_list = nil
+black_list = nil
+
+sync_lists = lambda do
+  content = Net::HTTP.get(URI.parse(WHITE_URI))
+  File.open(WHITE_LIST, 'w'){|file| file.write(content)}
+  white_list = FQDN::Database.new(WHITE_LIST)
+  content = Net::HTTP.get(URI.parse(GRAY_URI))
+  File.open(GRAY_LIST, 'w'){|file| file.write(content)}
+  gray_list = FQDN::Database.new(GRAY_LIST)
+  content = Net::HTTP.get(URI.parse(BLACK_URI))
+  File.open(BLACK_LIST, 'w'){|file| file.write(content)}
+  black_list = FQDN::Database.new(BLACK_LIST)
+end
+
+sync_lists.call
+
+# Sync the lists every SYNC_INTERVAL
+syncer = Thread.new do
+  loop do
+    sleep(SYNC_INTERVAL)
+    sync_lists.call
+  end
+end
+
+loop do
+  File.open(INPUT_LOG_FN) do |file|
+    while line = file.gets
+      # Drop partial logs
+      break unless line[-1, 1] == "\n"
+      fqdn = line.rstrip
+
+      # Do nothing with the FQDN in the white list
+      next if white_list.query(fqdn)
+
+      now = Time.new
+
+      label = black_list.query(fqdn)
+      if label
+        if now - label.ts > POST_INTERVAL
+          black_log.log(now, fqdn)
+          label.ts = now
+        end
+        next
+      end
+
+      label = gray_list.query(fqdn)
+      if label
+        if now - label.ts > POST_INTERVAL
+          gray_log.log(now, fqdn)
+          label.ts = now
+        end
+        next
+      else
+        gray_list.insert(fqdn, now)
+        gray_log.log(now, fqdn)
+      end
+    end
+  end
+end
